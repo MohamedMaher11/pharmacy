56024596290b21f8c9971a4992e05e6274269920
maher <maherrmaherr.gmail.com>
2024-08-19 02:03:38 +0300

    handle signout - loginuser

--- lib/Features/HomeScreen/views/Home.dart
+++ lib/Features/HomeScreen/views/Home.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hamo_pharmacy/Features/Chat/lastseen.dart';
 import 'package:hamo_pharmacy/Features/DetailsPage/medecindetails.dart';
+import 'package:hamo_pharmacy/Features/Doctor/doctorreservation.dart';
 import 'package:hamo_pharmacy/Features/HomeScreen/widgets/category.dart';
 import 'package:hamo_pharmacy/Features/HomeScreen/widgets/discount.dart';
 import 'package:hamo_pharmacy/Features/HomeScreen/widgets/doctor_consult.dart';
@@ -12,6 +13,7 @@ import 'package:hamo_pharmacy/Model/medecinmodel.dart';
 import 'package:hamo_pharmacy/Features/Cart/cartpage.dart';
 import 'package:hamo_pharmacy/Features/Favourate/favouratepage.dart';
 import 'package:hamo_pharmacy/gen/assets.gen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -29,6 +31,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+
     _fetchMedicines();
   }
 
@@ -269,66 +272,133 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Future<void> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+    if (isLoggedIn) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushReplacementNamed(context, '/home');
+      });
+    }
+  }
+
   Widget _buildDrawer(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text('اسم المستخدم'),
-            accountEmail: Text(_auth.currentUser?.email ?? ''),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://example.com/user_image.png'), // يجب استبدال هذا برابط صورة المستخدم
-            ),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('الرئيسية'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(FontAwesomeIcons.calendar),
-            title: Text('موعدي'),
-            onTap: () {
-              Navigator.of(context).pushNamed('/user_appointment');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text('المفضلة'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => FavoritesPage()),
+    final user = FirebaseAuth.instance.currentUser;
+
+    return FutureBuilder<DocumentSnapshot>(
+      future:
+          FirebaseFirestore.instance.collection('users').doc(user?.uid).get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Drawer(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        final userData = snapshot.data?.data() as Map<String, dynamic>?;
+        final userType = userData?[
+            'type']; // Assuming 'type' field is used to determine if the user is a doctor
+
+        return FutureBuilder<bool>(
+          future: _isDoctor(user?.uid),
+          builder: (context, doctorSnapshot) {
+            if (!doctorSnapshot.hasData) {
+              return Drawer(
+                child: Center(child: CircularProgressIndicator()),
               );
-            },
-          ),
-          ListTile(
-            leading: Icon(FontAwesomeIcons.message),
-            title: Text('الرسائل'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LastSeenPage()),
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('تسجيل الخروج'),
-            onTap: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.of(context).pushReplacementNamed('/sign_in');
-            },
-          ),
-        ],
-      ),
+            }
+
+            final isDoctor = doctorSnapshot.data ?? false;
+
+            return Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  UserAccountsDrawerHeader(
+                    accountName: Text('اسم المستخدم'),
+                    accountEmail: Text(user?.email ?? ''),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage('https://example.com/user_image.png'),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(FontAwesomeIcons.home),
+                    title: Text('الرئيسية'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  if (isDoctor) // Show this item if the user is a doctor
+                    ListTile(
+                      leading: Icon(FontAwesomeIcons.book),
+                      title: Text('حجوزاتي'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DoctorReservationsPage()),
+                        );
+                      },
+                    ),
+                  ListTile(
+                    leading: Icon(FontAwesomeIcons.calendar),
+                    title: Text('موعدي'),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/user_appointment');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(FontAwesomeIcons.heart),
+                    title: Text('المفضلة'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FavoritesPage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(FontAwesomeIcons.envelope),
+                    title: Text('الرسائل'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LastSeenPage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(FontAwesomeIcons.signOutAlt),
+                    title: Text('تسجيل الخروج'),
+                    onTap: () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      prefs.remove('isLoggedIn');
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.of(context).pushReplacementNamed('/sign_in');
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
+  }
+
+  Future<bool> _isDoctor(String? userId) async {
+    if (userId == null) return false;
+
+    final doctorDoc = await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(userId)
+        .get();
+    return doctorDoc.exists;
   }
 }
