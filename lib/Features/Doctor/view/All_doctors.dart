816acd31e905571154ee27b9633c8bebeb41dbd6
maher <maherrmaherr.gmail.com>
2024-08-27 01:34:52 +0300

    add favourate button on doctor card

---
+++ lib/Features/Doctor/view/All_doctors.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:hamo_pharmacy/core/valuenotifire.dart';
+import 'package:intl/intl.dart';
+import 'package:hamo_pharmacy/Features/Doctor/view/doctor_profile.dart';
+
+class AllDoctorsPage extends StatefulWidget {
+  final String specialty;
+
+  AllDoctorsPage({required this.specialty});
+
+  @override
+  _AllDoctorsPageState createState() => _AllDoctorsPageState();
+}
+
+class _AllDoctorsPageState extends State<AllDoctorsPage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  String _searchQuery = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '${widget.specialty} Doctors',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            _buildSearchBar(),
+            SizedBox(height: 40),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore
+                    .collection('doctors')
+                    .where('specialty', isEqualTo: widget.specialty)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  final doctors = snapshot.data!.docs.where((doc) {
+                    final doctorName = doc['name'].toString().toLowerCase();
+                    return doctorName.contains(_searchQuery.toLowerCase());
+                  }).toList();
+
+                  return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 40,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemCount: doctors.length,
+                    itemBuilder: (context, index) {
+                      final doctor = doctors[index];
+                      return _buildDoctorCard(context, doctor);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return TextField(
+      onChanged: (query) {
+        setState(() {
+          _searchQuery = query;
+        });
+      },
+      decoration: InputDecoration(
+        hintText: 'ابحث عن دكتور .....',
+        prefixIcon: Icon(FontAwesomeIcons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Colors.grey[200],
+      ),
+    );
+  }
+
+  Widget _buildDoctorCard(BuildContext context, DocumentSnapshot doctor) {
+    final doctorId = doctor.id;
+    final notifier = Provider.of<FavoritesNotifier>(context);
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DoctorProfilePage(doctor: doctor),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 4,
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: -30,
+                  left: 16,
+                  right: 16,
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.deepPurple,
+                    backgroundImage: NetworkImage(doctor['imageUrl']),
+                  ),
+                ),
+                Positioned(
+                  right: 8,
+                  top: 8,
+                  child: IconButton(
+                    icon: Icon(
+                      notifier.isFavorite(doctorId)
+                          ? FontAwesomeIcons.solidHeart
+                          : FontAwesomeIcons.heart,
+                      color: notifier.isFavorite(doctorId)
+                          ? Colors.red
+                          : Colors.black,
+                      size: 24,
+                    ),
+                    onPressed: () {
+                      notifier.toggleFavorite(doctorId);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        Text(
+                          doctor['name'],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey[800],
+                          ),
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          doctor['specialty'],
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[700],
+                          ),
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          doctor['education'] ?? 'Education not available',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[600],
+                          ),
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          doctor['address'] ?? 'Address not available',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[600],
+                          ),
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'سعر الحجز: ${NumberFormat.currency(symbol: 'جنيه ', decimalDigits: 0).format(doctor['consultationFee'])}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green[700],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
